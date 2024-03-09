eab8b55d32f4b1e9a9d646961b665e7d15b25dc3
amir <amirdehghani137722@gmail.com>
2024-03-09 09:33:28 +0330

    add risklevel color

--- src/components/LeafletMap/index.tsx
+++ src/components/LeafletMap/index.tsx
@@ -59,10 +59,12 @@ function Main(props: MainProps) {
       const markers = leaflet.markerClusterGroup({
         maxClusterRadius: 30,
         iconCreateFunction: function (cluster) {
-          const color =
+          console.log(cluster)
+          const risks = cluster.getAllChildMarkers().find(item => item.options.attribution == 'High')
+          let color =
             darkMode && colorScheme
-              ? getColor("darkmode.100", 0.6)
-              : getColor("primary", 0.8);
+              ? getColor(risks?'red.500':"darkmode.100", 0.6)
+              : getColor(risks?'red.500':"primary", 0.8);
           const mapMarkerRegionSvg =
             window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="55.066" height="47.691" viewBox="0 0 55.066 47.691">
                   <g id="Group_15" data-name="Group 15" transform="translate(-319.467 -83.991)">
@@ -85,7 +87,6 @@ function Main(props: MainProps) {
                   </g>
                 </svg>
               `);
-
           return leaflet.divIcon({
             html: `<div class="relative w-full h-full">
                     <div class="absolute inset-0 flex items-center justify-center ml-1.5 mb-0.5 font-medium text-white">${cluster.getChildCount()}</div>
@@ -102,12 +103,23 @@ function Main(props: MainProps) {
 
       map.addLayer(markers);
 
-      const color =
+      let color =
         darkMode && colorScheme
-          ? getColor("darkmode.100")
-          : getColor("primary");
+          ? getColor("gray.100",0.9)
+          : getColor("primary",0.9);
       const mapMarkerSvg =
         window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="31.063" viewBox="0 0 20 31.063">
+                <g id="Group_16" data-name="Group 16" transform="translate(-408 -150.001)">
+                  <path id="Subtraction_21" data-name="Subtraction 21" d="M10,31.064h0L1.462,15.208A10,10,0,1,1,20,10a9.9,9.9,0,0,1-1.078,4.522l-.056.108c-.037.071-.077.146-.121.223L10,31.062ZM10,2a8,8,0,1,0,8,8,8,8,0,0,0-8-8Z" transform="translate(408 150)" fill="${color}"/>
+                  <circle id="Ellipse_26" data-name="Ellipse 26" cx="6" cy="6" r="6" transform="translate(412 154)" fill="${color}"/>
+                </g>
+              </svg>
+            `);
+    color =
+        darkMode && colorScheme
+          ? getColor("red.500")
+          : getColor("red.500");        
+    const mapMarkerSvgRed = window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="31.063" viewBox="0 0 20 31.063">
                 <g id="Group_16" data-name="Group 16" transform="translate(-408 -150.001)">
                   <path id="Subtraction_21" data-name="Subtraction 21" d="M10,31.064h0L1.462,15.208A10,10,0,1,1,20,10a9.9,9.9,0,0,1-1.078,4.522l-.056.108c-.037.071-.077.146-.121.223L10,31.062ZM10,2a8,8,0,1,0,8,8,8,8,0,0,0-8-8Z" transform="translate(408 150)" fill="${color}"/>
                   <circle id="Ellipse_26" data-name="Ellipse 26" cx="6" cy="6" r="6" transform="translate(412 154)" fill="${color}"/>
@@ -122,8 +134,9 @@ function Main(props: MainProps) {
           },
           {
             title: markerElem.name,
+            attribution:markerElem.riskLevel,
             icon: leaflet.icon({
-              iconUrl: `data:image/svg+xml;base64,${mapMarkerSvg}`,
+              iconUrl: `data:image/svg+xml;base64,${markerElem.riskLevel == 'High' ?mapMarkerSvgRed :mapMarkerSvg}`,
               iconAnchor: leaflet.point(10, 35),
             }),
           }
